ee60a4344afecd19ed7701cfaf6af8d5dc982aa2
Nikita Bobko <git@bobko.xyz>
2026-04-25 20:42:34 +0200

    minSdk = 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     defaultConfig {
         applicationId = "bobko.stitch_screenshots"
-        minSdk = 26
+        minSdk = 23
         targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"

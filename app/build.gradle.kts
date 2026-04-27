b62fcfae4733df1b87f62b6eec0237de23b1b76f
Nikita Bobko <git@bobko.xyz>
2026-04-27 13:22:26 +0200

    Drop unnecessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,14 +75,8 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.04.01"))
-    implementation("androidx.core:core-ktx:1.18.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("io.coil-kt:coil-compose:2.7.0")
 }
 
 kotlin {
